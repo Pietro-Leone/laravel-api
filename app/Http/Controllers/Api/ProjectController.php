c7de9c03380da5bbae9f8246bffb79150b6d3e20
Pietro-Leone <leonepietro1999@gmail.com>
2023-10-17 17:09:14 +0200

    add show api

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -9,8 +9,16 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function index(){
-        $projects = Project::with(["technologies", "type"])->paginate(5);
+        $projects = Project::with(["technologies", "type"])->paginate(6);
 
         return response()->json($projects);
+    }
+
+    public function show($slug) {
+        $project = Project::where("slug", $slug)
+            ->with(["technologies", "type"])
+            ->first();
+
+        return response()->json($project);
     }
 }
